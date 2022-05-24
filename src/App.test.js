559f0d5c84c2c7cb1edc9c2089a3cf7e30174062
Alex <akomanov@seatgeek.com>
2022-05-24 19:01:55 +0300

    Adding additional components tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('learn-link')
   expect(linkElement).toBeInTheDocument();
+});
+
+test('has a correct link', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('learn-link')
+  expect(linkElement.href).toContain('https://reactjs.org');
+});
+
+test('has a correct text', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('learn-link')
+  expect(linkElement).toHaveTextContent('Learn Automation Testing and Dev');
 });
